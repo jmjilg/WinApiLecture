b6594316e551fe68f46203ffbb63eb545555923a
전민종 <giml790880@gmail.com>
2022-11-20 12:17:09 +0900

    Win32API 강의 49화. Win32API UI (3)

--- Client/CUIMgr.h
+++ Client/CUIMgr.h
@@ -1,5 +1,7 @@
 #pragma once
 
+class CUI;
+
 class CUIMgr
 {
 	SINGLE(CUIMgr);
@@ -8,6 +10,10 @@ private:
 
 public:
 	void update();
+
+private:
+	// 부모 UI 내에서 실제로 타겟팅 된 UI 를 찾아서 반환한다.
+	CUI* GetTargetedUI(CUI* _pParentUI);	
 
 };
 
