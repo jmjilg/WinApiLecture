91baa4e45914808d6d4cb769b9241e74aecc3f71
전민종 <giml790880@gmail.com>
2022-11-21 19:14:34 +0900

    Win32API 강의 51화. Win32API UI (5)

---
+++ Client/CBtnUI.cpp
@@ -0,0 +1,22 @@
+#include "CBtnUI.h"
+
+CBtnUI::CBtnUI()
+	: CUI(false)
+{
+}
+
+CBtnUI::~CBtnUI()
+{
+}
+
+void CBtnUI::MouseOn()
+{
+}
+
+void CBtnUI::MouseLbtnDown()
+{
+}
+
+void CBtnUI::MouseLbtnUp()
+{
+}
---
+++ Client/CBtnUI.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "CUI.h"
+class CBtnUI :
+    public CUI
+{
+public:
+    virtual void MouseOn();
+    virtual void MouseLbtnDown();
+    virtual void MouseLbtnUp();
+
+    CLONE(CBtnUI);
+
+public:
+    CBtnUI();
+    ~CBtnUI();
+};
+
