b56af5986a03cb861fdac49038e1d7bcd1df5c6b
전민종 <giml790880@gmail.com>
2022-12-23 15:57:30 +0900

    Win32API 강의 67화. Win32API RigidBody (4)

--- Client/CPlayer.cpp
+++ Client/CPlayer.cpp
@@ -18,6 +18,8 @@
 #include "CRigidBody.h"
 
 CPlayer::CPlayer()
+	: m_eCurState(PLAYER_STATE::IDLE)
+	, m_iDir(1)
 {
 	// Texture 로딩하기
 	//m_pTex = CResMgr::GetInst()->LoadTexture(L"PlayerTex", L"texture\\Player.bmp");
@@ -30,15 +32,17 @@ CPlayer::CPlayer()
 	CreateRigidBody();
 
 	// Texture 로딩하기
-	CTexture* pTex = CResMgr::GetInst()->LoadTexture(L"PlayerTex", L"texture\\link_0.bmp");
+	CTexture* pTex = CResMgr::GetInst()->LoadTexture(L"PlayerLeft", L"texture\\Player01.bmp"); // 나는 슈퍼 메트로이드의 사무스 아란 스프라이트를 사용했음
+	//CTexture* pRightTex = CResMgr::GetInst()->LoadTexture(L"PlayerRight", L"texture\\Player01_R.bmp");
 
 	CreateAnimator();	
-	GetAnimator()->CreateAnimation(L"WALK_DOWN", pTex, Vec2(0.f, 520.f), Vec2(120.f, 130.f), Vec2(120.f, 0.f), 0.1f, 10);
-	GetAnimator()->Play(L"WALK_DOWN", true);
+	GetAnimator()->CreateAnimation(L"IDLE_RIGHT", pTex, Vec2(19.f, 179.f), Vec2(80.f, 80.f), Vec2(96.f, 0.f), 0.1f, 4);
+	GetAnimator()->CreateAnimation(L"IDLE_LEFT", pTex, Vec2(19.f, 307.f), Vec2(80.f, 80.f), Vec2(96.f, 0.f), 0.1f, 4);
 
-	CAnimation* pAnim = GetAnimator()->FindAnimation(L"WALK_DOWN");
-	for (UINT i = 0; i < pAnim->GetMaxFrame(); ++i)
-		pAnim->GetFrame(i).vOffset = Vec2(0.f, -20.f);
+	GetAnimator()->CreateAnimation(L"WALK_LEFT", pTex, Vec2(19.f, 1331.f), Vec2(80.f, 80.f), Vec2(96.f, 0.f), 0.1f, 10);
+	GetAnimator()->CreateAnimation(L"WALK_RIGHT", pTex, Vec2(19.f, 1203.f), Vec2(80.f, 80.f), Vec2(96.f, 0.f), 0.1f, 10);
+		
+	GetAnimator()->Play(L"WALK_LEFT", true);
 }
 
 CPlayer::~CPlayer()
@@ -48,47 +52,14 @@ CPlayer::~CPlayer()
 
 void CPlayer::update()
 {
-	CRigidBody* pRigid = GetRigidBody();
+	m_ePrevState = m_eCurState;
 
-	if (KEY_HOLD(KEY::W))
-	{
-		pRigid->AddForce(Vec2(0.f, -200.f));
-	}
+	update_move();
 
-	if (KEY_HOLD(KEY::S))
-	{
-		pRigid->AddForce(Vec2(0.f, 200.f));
-	}
+	update_state();
 
-	if (KEY_HOLD(KEY::A))
-	{
-		pRigid->AddForce(Vec2(-200.f, 0.f));
-	}
+	update_animation();
 
-	if (KEY_HOLD(KEY::D))
-	{
-		pRigid->AddForce(Vec2(200.f, 0.f));
-	}
-
-	if (KEY_TAP(KEY::W))
-	{
-		pRigid->AddVelocity(Vec2(0.f, -100.f));
-	}
-
-	if (KEY_TAP(KEY::S))
-	{
-		pRigid->AddVelocity(Vec2(0.f, 100.f));
-	}
-
-	if (KEY_TAP(KEY::A))
-	{
-		pRigid->AddVelocity(Vec2(-100.f, 0.f));
-	}
-
-	if (KEY_TAP(KEY::D))
-	{
-		pRigid->AddVelocity(Vec2(100.f, 0.f));
-	}
 
 	if (KEY_TAP(KEY::SPACE))
 	{
@@ -163,4 +134,87 @@ void CPlayer::CreateMissile()
 	pMissile->SetDir(Vec2(0.f, -1.f));
 	
 	CreateObject(pMissile, GROUP_TYPE::PROJ_PLAYER); // 앞으로 생성될 모든 종류의 오브젝트를 커버할 수 있어야함
+}
+
+void CPlayer::update_state()
+{
+	if (KEY_TAP(KEY::A))
+	{
+		m_iDir = -1;
+		m_eCurState = PLAYER_STATE::WALK;
+	}
+
+	if (KEY_TAP(KEY::D))
+	{
+		m_iDir = 1;
+		m_eCurState = PLAYER_STATE::WALK;
+	}
+}
+
+void CPlayer::update_move()
+{
+	CRigidBody* pRigid = GetRigidBody();
+
+	if (KEY_HOLD(KEY::W))
+	{
+		pRigid->AddForce(Vec2(0.f, -200.f));
+	}
+
+	if (KEY_HOLD(KEY::S))
+	{
+		pRigid->AddForce(Vec2(0.f, 200.f));
+	}
+
+	if (KEY_HOLD(KEY::A))
+	{
+		pRigid->AddForce(Vec2(-200.f, 0.f));
+	}
+
+	if (KEY_HOLD(KEY::D))
+	{
+		pRigid->AddForce(Vec2(200.f, 0.f));
+	}
+
+	if (KEY_TAP(KEY::W))
+	{
+		pRigid->AddVelocity(Vec2(0.f, -100.f));
+	}
+
+	if (KEY_TAP(KEY::S))
+	{
+		pRigid->AddVelocity(Vec2(0.f, 100.f));
+	}
+
+	if (KEY_TAP(KEY::A))
+	{
+		pRigid->AddVelocity(Vec2(-100.f, 0.f));
+	}
+
+	if (KEY_TAP(KEY::D))
+	{
+		pRigid->AddVelocity(Vec2(100.f, 0.f));
+	}
+}
+
+void CPlayer::update_animation()
+{
+	if (m_ePrevState == m_eCurState)
+		return;
+
+
+	switch (m_eCurState)
+	{
+	case PLAYER_STATE::IDLE:
+
+		break;
+	case PLAYER_STATE::WALK:
+
+		break;
+	case PLAYER_STATE::ATTACK:
+
+		break;
+	case PLAYER_STATE::DEAD:
+
+		break;
+	}
 }
--- Client/CPlayer.h
+++ Client/CPlayer.h
@@ -1,6 +1,28 @@
 #pragma once
 #include "CObject.h"
 
+
+
+enum class PLAYER_STATE
+{
+    IDLE,
+    WALK,
+    ATTACK,
+    DEAD,
+};
+
+
+
+enum class PLAYER_ATTACK
+{
+    NORMAL_ATT_1,
+    NORMAL_ATT_2,
+    NORMAL_ATT_3,
+
+    SKILL_ATT_1,
+    // ....
+};
+
 class CTexture;
 
 class CPlayer :
@@ -9,13 +31,19 @@ class CPlayer :
 private:
     vector<CObject*>    m_vecColObj;
 
+    PLAYER_STATE        m_eCurState;
+    PLAYER_STATE        m_ePrevState;
+    int                 m_iDir;
+
 public:
     virtual void update();
     virtual void render(HDC _dc);
 
 private:
     void CreateMissile();
-
+    void update_state();
+    void update_move();
+    void update_animation();
 
     CLONE(CPlayer);
 
